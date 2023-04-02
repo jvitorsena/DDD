6d46ea5b52b6170dc1d52716efafc6cf6b3963da
João Vitor <jvitorsena96@gmail.com>
2023-04-02 19:24:38 -0300

    :sparkles: Inicio teste unitario

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,40 @@
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Data.Test;
+
+public abstract class BaseTest
+{
+    public BaseTest()
+    {
+        
+    }
+}
+
+public class DbTeste : IDisposable
+{
+    private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+    public ServiceProvider ServiceProvider { get; private set; }
+
+    public DbTeste()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddDbContext<MyContext>(o =>
+            o.UseMySql($"Persist Security Info=True;server=localhost;user=root;password=root_pwd;database={dataBaseName}", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql")),
+            ServiceLifetime.Transient
+            );
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+        using (var context = ServiceProvider.GetService<MyContext>())
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+    public void Dispose()
+    {
+        using (var context = ServiceProvider.GetService<MyContext>())
+        {
+            context.Database.EnsureDeleted();
+        }
+    }
+}
--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Api.Domain.Entities;
 using Api.Data.Maping;
+using Api.Domain.Dtos.User;
 
 namespace Api.Data.Context
 {
@@ -17,6 +18,15 @@ namespace Api.Data.Context
         {
             base.OnModelCreating(modelbuilder);
             modelbuilder.Entity<UserEntity>(new UserMap().Configure);
+            modelbuilder.Entity<UserEntity>().HasData(
+                new UserEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Administrador",
+                    Email = "email@email.com.br",
+                    CreateAt = DateTime.Now,
+                    updateAt = DateTime.Now
+                });
         }
     }
 }
